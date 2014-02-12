317ada375d4b156f803f7c7e6c7a8dcbb8aabc35
Alumno <alumno@localhost.localdomain>
2014-02-12 08:29:20 -0600

    examen

---
+++ src/nely/miclase.java
@@ -0,0 +1,110 @@
+package nely;
+
+import java.util.Scanner;
+
+public class miclase
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+//		Scanner mi_Scanner=new Scanner(System.in);
+//		
+//		for(int i=0 ; i < 5 ; i++)
+//		{
+//			int x= mi_Scanner.nextInt();	
+//			if (x==10) 
+//			{
+//				System.out.print("Es igual a 10");
+//			}
+//			else
+//			{
+//				System.out.print("Es diferente a 10");
+//			}
+//		}
+//        Scanner mi_Scanner=new Scanner(System.in);
+
+		//	for(int i=0 ; i<3 ; i=i+1)
+		//{
+	//		System.out.print("ingrese nota");
+	//		int nota = mi_Scanner.nextInt();
+	//		if (nota >=90)
+	//		{
+	//			System.out.print("exelencia");
+		//	}
+	//		else if (nota >=80)
+	//		{
+	//			System.out.print("sobresaliente");
+	//		}
+	//		else if (nota>=60)
+	//		{
+	//			System.out.print("aprobado");
+	//		}else
+	//		{
+	//			System.out.print("reprobado");
+	//		}
+	//	}		
+//	}
+
+	
+		Scanner mi_Scanner=new Scanner(System.in);
+		double num1,num2;
+		System.out.print("introduzca primer numero");
+		num1= mi_Scanner.nextDouble();
+		System.out.print("introduzca segundo numero");
+		num2= mi_Scanner.nextDouble();
+		System.out.print("ingrese 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir: ");
+		{	
+			int x= mi_Scanner.nextInt();
+			if (x==1)
+			{
+				System.out.print("la suma es: ");
+				System.out.println(num1 + num2);
+			}
+			
+			
+			if (x==2)
+			{
+				System.out.print("la resta es: ");
+				System.out.println(num1-num2);
+			}	
+			
+	            if (x==3)
+	        {
+	            System.out.print("la multiplicacion es: ");
+	            System.out.println(num1*num2);
+			}
+			
+				
+			int xx = mi_Scanner.nextInt();
+			if (x==4);
+		    }
+			    System.out.print("la divicion es: ");
+			    System.out.println(num1/num2);
+			}
+}
+
+	
+
+			
+			
+				
+			
+		
+		
+		
+			
+				
+			
+		
+		
+		
+				
+			
+				
+				
+				
+		
+	
+		
+		
